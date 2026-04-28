1638699dd264156282f1fcb77ef85de9891984bf
Tasavvuf Gori <tasavvufg@gmail.com>
2026-04-28 12:22:02 +0530

    managed to hide the uri of mongo db

--- MONGODB/notes/src/db/db.js
+++ MONGODB/notes/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
-
+require("dotenv").config()
 async function conectDB() {
-    await mongoose.connect("mongodb+srv://tevmusix_db_user:9yvPlvxJWSDLOYe4@cluster0.15ujrpm.mongodb.net/newdb")
+    await mongoose.connect(process.env.DB_URI)
     console.log("db has been connected ")
 }
 module.exports = conectDB
