cc69bcf07ae64e0560021485f76ec21700505f54
Tasavvuf Gori <tasavvufg@gmail.com>
2026-06-18 18:04:06 +0530

    problem 2 : Server sends back to client ⭐⭐ Goal: Client sends message → Server receives → Server sends response back.

--- socket.io/backend/server.js
+++ socket.io/backend/server.js
@@ -1,29 +1,34 @@
-const console = require('console')
-const express = require('express')
-const http = require('http')
-const { Server } = require('socket.io')
+const console = require("console");
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
-const app = express()
+const app = express();
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-const io = new Server(server ,{
-    cors:{origin:"*"}
-} )
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
 
-app.get("/",(_,res)=>{
-    res.send("server is running yes")
-})
-io.on("connection",(socket)=>{
+app.get("/", (_, res) => {
+  res.send("server is running yes");
+});
+io.on("connection", (socket) => {
+  console.log("user connected user id: ", socket.id);
 
-    console.log("user connected user id: ",socket.id)
-    socket.on("message",(data)=>{
-        console.log(data)
-    })
-    socket.on("disconnect", () => {
-        console.log("User disconnected:", socket.id)
-    })
-})
+  socket.on("message", (data) => {
+    console.log(data);
+
+    socket.emit("receive_message", {
+      text: "server gets your message " + data.text,
+      timestamp: new Date().toLocaleTimeString(),
+    });
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
 server.listen(3000, () => {
-    console.log("Server running on port 3000")
-})
+  console.log("Server running on port 3000");
+});
