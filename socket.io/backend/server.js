92a651dd23d561ef7b2c1f63c2b4926949e5880e
Tasavvuf Gori <tasavvufg@gmail.com>
2026-06-18 17:28:05 +0530

    add initial Socket.io server and frontend setup , this time we are taking the both separted

---
+++ socket.io/backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+
+const app = express()
+
+const server = http.createServer(app)
+
+const io = new Server(server ,{
+    cors:{origin:"*"}
+} )
+
+app.get("/",(_,res)=>{
+    res.send("server is running yes")
+})
+io.on("connection",(socket)=>{
+
+    console.log("user connected user id: ",socket.id)
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id)
+    })
+})
+server.listen(3000, () => {
+    console.log("Server running on port 3000")
+})
