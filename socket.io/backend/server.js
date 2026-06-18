8f214c98e9555b8bfcc6785e333bfc5cbb620d87
Tasavvuf Gori <tasavvufg@gmail.com>
2026-06-18 23:47:53 +0530

    PROBLEM 4: Rooms ⭐⭐⭐ Goal: Users join specific rooms, messages only go to that room. Real world: WhatsApp group. Only group members see messages.

--- socket.io/backend/server.js
+++ socket.io/backend/server.js
@@ -16,18 +16,29 @@ app.get("/", (_, res) => {
 });
 io.on("connection", (socket) => {
   console.log("user connected user id: ", socket.id);
+  socket.on("join_room", (room) => {
+    socket.join(room);
+
+    console.log(room);
+    console.log(`${socket.id} joined room : ${room}`);
+    
+  });
   try {
       socket.on("message", (data) => {
-    console.log("data was reached to server" ,data);
-
-    io.emit("rmessage", {
-       ...data
-    })
+        console.log("data was reached to server", data);
+          io.to(data.room).emit("rmessage", {
+    ...data,
   });
-  } catch (error) {
-    console.log(error)
-  }
 
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+socket.on("leave_room", (room) => {
+    socket.leave(room);
+    console.log(`${socket.id} left ${room}`);
+});
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
   });
