4ef84c28f48530d16a4cfbefd1b912f940d7e089
Tasavvuf Gori <tasavvufg@gmail.com>
2026-06-19 22:01:51 +0530

    PROBLEM 5: Sending Location Data ⭐⭐⭐ Goal: Client sends coordinates → Server broadcasts to all → Others receive location

--- socket.io/backend/server.js
+++ socket.io/backend/server.js
@@ -15,34 +15,25 @@ app.get("/", (_, res) => {
   res.send("server is running yes");
 });
 io.on("connection", (socket) => {
-  console.log("user connected user id: ", socket.id);
-  socket.on("join_room", (room) => {
-    socket.join(room);
+    console.log("Connected:", socket.id)
 
-    console.log(room);
-    console.log(`${socket.id} joined room : ${room}`);
-    
-  });
-  try {
-      socket.on("message", (data) => {
-        console.log("data was reached to server", data);
-          io.to(data.room).emit("rmessage", {
-    ...data,
-  });
+    socket.on("locationGet", (data) => {
+        console.log(`Location from ${socket.id}:`, data)
+        
+        io.emit("BroadcastingLocationData", {
+            id: socket.id,
+            lat: data.lat,
+            log: data.log,
+            timestamp: new Date().toLocaleTimeString()
+        })
+    })
 
-      });
-    } catch (error) {
-      console.log(error);
-    }
-
-socket.on("leave_room", (room) => {
-    socket.leave(room);
-    console.log(`${socket.id} left ${room}`);
-});
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
-});
+    socket.on("disconnect", () => {
+        // Tell everyone this user disconnected
+        io.emit("user_disconnected", { id: socket.id })
+        console.log("Disconnected:", socket.id)
+    })
+})
 server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
